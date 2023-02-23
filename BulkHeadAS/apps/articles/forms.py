e590f5b894a42758b4ce52ec1e0b3d2868613372
Alexei <alexeirubers23@gmail.com>
2023-02-23 12:54:55 +0300

    update front and back-end form

--- BulkHeadAS/apps/articles/forms.py
+++ BulkHeadAS/apps/articles/forms.py
@@ -2,14 +2,31 @@ from django import forms
 from django.forms import ModelForm
 from .models import *
 
-phone_regex = r'\+375(17|29|25|33|44)[1-9]{7}'
+phone_regex = r'\+375(17|29|25|33|44)[0-9]{7}'
 regex_error = 'Телефонный номер должен быть в формате: +375(17/25/29/33/44)1234567.'
 
 CARS = [
     ('Toyota', (('C-HR', 'C-HR'), ('Sai', 'Sai'), ('Auris', 'Auris'), ('Prius', 'Prius'), ('Land Cruiser', 'Land Cruiser'),
         ('Avensis', 'Avensis'), ('Camry', 'Camry'), ('Highlander', 'Highlander'), ('RAV4', 'RAV4'), ('Crown', 'Crown')
-    )
+                )
      ),
+    ('Renault', (('Latitude', 'Latitude'), ('Koleos', 'Koleos'), ('Arkana', 'Arkana'), ('Kaptur', 'Kaptur'), ('Fluence', 'Fluence'),
+        ('Grand Scenic', 'Grand Scenic'), ('Laguna', 'Laguna'), ('Trafic', 'Trafic'), ('Logan Stepway', 'Logan Stepway'), ('Duster', 'Duster')
+                )
+     ),
+    ('Opel', (('Insignia', 'Insignia'), ('Mokka', 'Mokka'), ('Astra GTC', 'Astra GTC'), ('Antara', 'Antara'), ('Astra Family', 'Astra Family'),
+        ('Astra', 'Astra'), ('Zafira', 'Zafira'), ('Vivaro', 'Vivaro'), ('Corsa', 'Corsa'), ('Meriva', 'Meriva')
+                )
+     ),
+    ('Honda', (('Legend', 'Legend'), ('Shuttle', 'Shuttle'), ('N-WGN', 'N-WGN'), ('Crosstour', 'Crosstour'), ('Civic Type R', 'Civic Type R'),
+        ('Vezel', 'Vezel'), ('CR-Z', 'CR-Z'), ('Jade', 'Jade'), ('Fit Shuttle', 'Fit Shuttle'), ('Freed Spike', 'Freed Spike')
+                )
+     ),
+    ('BMW', (('6-Series', '6-Series'), ('X6', 'X6'), ('X5', 'X5'), ('5-Series Gran Turismo', '5-Series Gran Turismo'), ('X4', 'X4'),
+        ('X3', 'X3'), ('7-Series', '7-Series'), ('3-Series Gran Turismo', '3-Series Gran Turismo'), ('5-Series', '5-Series'), ('Z4', 'Z4')
+                )
+     )
+
 ]
 
 class Entrollment(ModelForm):
@@ -18,7 +35,7 @@ class Entrollment(ModelForm):
     car = forms.ChoiceField(choices = CARS, label='Автомобиль')
     phone_number = forms.RegexField(regex=phone_regex,
                                     label='Номер телефона',
-                                    help_text="Телефонный номер должен быть в формате: +375(17/25/29/33/44)XXXXXXX."
+                                    help_text="Телефонный номер должен быть в формате: +375(17/25/29/33/44)XXXXXXX.",
                                     )
     client_service = models.ForeignKey(Services, on_delete=models.CASCADE, default=1)
     pub_order = forms.DateTimeField(label="Запись на")
--- BulkHeadAS/apps/articles/models.py
+++ BulkHeadAS/apps/articles/models.py
@@ -7,8 +7,25 @@ from django.db import models
 CARS = [
     ('Toyota', (('C-HR', 'C-HR'), ('Sai', 'Sai'), ('Auris', 'Auris'), ('Prius', 'Prius'), ('Land Cruiser', 'Land Cruiser'),
         ('Avensis', 'Avensis'), ('Camry', 'Camry'), ('Highlander', 'Highlander'), ('RAV4', 'RAV4'), ('Crown', 'Crown')
-    )
+                )
      ),
+    ('Renault', (('Latitude', 'Latitude'), ('Koleos', 'Koleos'), ('Arkana', 'Arkana'), ('Kaptur', 'Kaptur'), ('Fluence', 'Fluence'),
+        ('Grand Scenic', 'Grand Scenic'), ('Laguna', 'Laguna'), ('Trafic', 'Trafic'), ('Logan Stepway', 'Logan Stepway'), ('Duster', 'Duster')
+                )
+     ),
+    ('Opel', (('Insignia', 'Insignia'), ('Mokka', 'Mokka'), ('Astra GTC', 'Astra GTC'), ('Antara', 'Antara'), ('Astra Family', 'Astra Family'),
+        ('Astra', 'Astra'), ('Zafira', 'Zafira'), ('Vivaro', 'Vivaro'), ('Corsa', 'Corsa'), ('Meriva', 'Meriva')
+                )
+     ),
+    ('Honda', (('Legend', 'Legend'), ('Shuttle', 'Shuttle'), ('N-WGN', 'N-WGN'), ('Crosstour', 'Crosstour'), ('Civic Type R', 'Civic Type R'),
+        ('Vezel', 'Vezel'), ('CR-Z', 'CR-Z'), ('Jade', 'Jade'), ('Fit Shuttle', 'Fit Shuttle'), ('Freed Spike', 'Freed Spike')
+                )
+     ),
+    ('BMW', (('6-Series', '6-Series'), ('X6', 'X6'), ('X5', 'X5'), ('5-Series Gran Turismo', '5-Series Gran Turismo'), ('X4', 'X4'),
+        ('X3', 'X3'), ('7-Series', '7-Series'), ('3-Series Gran Turismo', '3-Series Gran Turismo'), ('5-Series', '5-Series'), ('Z4', 'Z4')
+                )
+     )
+
 ]
 class Services(models.Model):
     service_name = models.CharField('Название услуги', max_length=50, default='')
@@ -24,7 +41,7 @@ class Services(models.Model):
 class Article(models.Model):
 
     fio = models.CharField('ФИО', max_length=30, default='Рубец Алексей Сергеевич')
-    car = models.CharField('Автомобиль', max_length=15, choices=CARS, default='Не указан')
+    car = models.CharField('Автомобиль', max_length=30, choices=CARS, default='Не указан')
 
     phone_number = models.CharField('Телефонный номер',
                                     validators=[RegexValidator(r'\+375(17|29|25|33|44)[0-9]{7}')],
