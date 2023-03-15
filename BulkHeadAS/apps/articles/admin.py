ef9ff43dc74baa70ec9bb9018ff50a2dcb38df5f
Alexei <alexeirubers23@gmail.com>
2023-03-15 10:17:01 +0300

    update admin.py models

--- BulkHeadAS/apps/articles/admin.py
+++ BulkHeadAS/apps/articles/admin.py
@@ -4,6 +4,3 @@ admin.site.register(models.Article)
 admin.site.register(models.Services)
 admin.site.register(models.Employee)
 admin.site.register(models.Work)
-
-
-""" Александр Миско мой любимый преподаватель, жаль, что он у нас не вел великолепный Framework Django! """
