000591f3d7aa161eab622cb3353ceaee3a4937ad
Alexei <alexeirubers23@gmail.com>
2023-04-06 08:47:02 +0300

    change database file

--- BulkHeadAS/settings.py
+++ BulkHeadAS/settings.py
@@ -89,7 +89,7 @@ WSGI_APPLICATION = 'BulkHeadAS.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'bulk.sqlite3',
     }
 }
 
