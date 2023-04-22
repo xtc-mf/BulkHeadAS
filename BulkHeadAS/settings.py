46077b1174da3c24c867017b9bdb0c3df00cfb24
Alexei <alexeirubers23@gmail.com>
2023-04-22 12:17:14 +0300

    avoid VeraCrypt

--- BulkHeadAS/settings.py
+++ BulkHeadAS/settings.py
@@ -134,3 +134,9 @@ STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
